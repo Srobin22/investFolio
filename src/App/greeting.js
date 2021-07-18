3c89846bf4c0b26ba3a13c8e14db8e2830003d66
robin <shamimbabul1118@gmail.com>
2021-07-18 12:43:19 -0400

    added app layout

---
+++ src/App/greeting.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Greeting (props) {
+    return <h1>Welcome to CryptoApp</h1>;
+  }
--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,26 @@
 import React,{Component} from 'react';
 import './App.css';
-
+import AppLayout from './AppLayout'
+import Greeting from './greeting';
+import styled,{css}from 'styled-components';
+const MyButton=styled.div`
+  color:green;
+  ${props => props.primary && css`
+  color: blue;
+`}
+`
+const TomatoButton=styled(MyButton)`
+  color:tomato;
+  border-color: tomato;
+`
 class App extends Component{
   render(){
     return(
-      <div>Hello World</div>
+      <AppLayout>
+        <Greeting/>
+
+      </AppLayout>
+      
     );
   }
 }
